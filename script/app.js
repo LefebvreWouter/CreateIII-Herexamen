63ce9d51428ebac2d25763ed7dda8c0e261a8060
LefebvreWouter <wouter.lefebvre@student.howest.be>
2018-07-26 08:20:03 +0200

    2nd Push

---
+++ script/app.js
@@ -0,0 +1,15 @@
+"use strict"
+
+console.log('document is loaded !!!')
+
+// All Worldcup teams full group data
+function GetJSON(){
+  let url = "https://worldcup.sfg.io/teams/"
+  let teams = document .getElementById('')
+
+
+
+
+
+
+}
